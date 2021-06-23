d071e940f6ba4b41525338f77c15c03b7438d263
Luan Barbosa <luan.tads@gmail.com>
2021-06-23 03:03:44 -0300

    Second  commit Created Migrations

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,10 @@
+import "reflect-metadata";
 import express from "express";
 
-//@types/express
+import "./database";
+
 const app = express();
 
-/**
- * GET    => Buscar uma informação.
- * POST   => Inserir (Criar) uma informação.
- * PUT    => Alterar uma informação.
- * DELETE => Remover um dado.
- * PATCH  => Alterar uma informação específica.
- */
-
-app.get("/test", (request, Response) => {
-  //Request => Entrando
-  //Response => Saindo
-  return Response.send("Olá NLW 06!");
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método POST");
-});
 
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running NLW 06!"));
